c0f0c0e2b4e341b7bb19cf0f54813eca2e786150
Kostas <kostasspanolios@gmail.com>
2022-10-25 20:19:43 +0100

    add properties, add pages

---
+++ EmployeeDetails/Data/EmployeeProperties.cs
@@ -0,0 +1,13 @@
+namespace BlazorApp1.Data
+{
+    public class EmployeeProperties
+    {
+        //no need for a setter in this user story
+        public String? Name { get; }
+
+        public String? Position { get; }
+
+        //to be changed
+        public String? Skills { get; }
+    }
+}
